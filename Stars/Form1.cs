6d91474a4dba839150ec924604aeb0a682490409
Nino van der Mark <ninovdmark@gmail.com>
2021-08-29 19:03:56 +0200

    Committed source files

---
+++ Stars/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+        }
+
+        List<GfxStar> stars = new List<GfxStar>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            Cursor.Clip = this.Bounds;
+            DrawTimer.Interval = 1000 / 60;
+
+            InitStars();
+        }
+
+        private void InitStars()
+        {
+            Random random = new Random();
+            Color newColor = Color.FromArgb(255,255,255);
+            Point newLocation = new Point(0, 0);
+            GfxStar.StarClasses newClass = GfxStar.StarClasses.A;
+
+            for (int t = 0; t < 5000; t++)
+            {
+                newLocation.X = random.Next(0, this.Width);
+                newLocation.Y = random.Next(0, this.Height);
+
+                // Respects the distribution according to the Journal of the Royal Astronomical Society of Canada
+                int classRandom = random.Next(0, 10000);
+                if (classRandom < 1) { newClass = GfxStar.StarClasses.O; }
+                else if (classRandom <= 13) { newClass = GfxStar.StarClasses.B; }
+                else if (classRandom <= 60) { newClass = GfxStar.StarClasses.A; }
+                else if (classRandom <= 300) { newClass = GfxStar.StarClasses.F; }
+                else if (classRandom <= 760) { newClass = GfxStar.StarClasses.G; }
+                else if (classRandom <= 1210) { newClass = GfxStar.StarClasses.K; }
+                else { newClass = GfxStar.StarClasses.M; }
+
+                stars.Add(new GfxStar(newLocation, newClass));
+            }
+        }
+
+        private void DrawTimer_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+
+            foreach (GfxStar star in stars)
+            {
+                star.Draw(e.Graphics);
+            }
+        }
+    }
+}
---
+++ Stars/GfxStar.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars
+{
+    class GfxStar
+    {
+        private Point location;
+        private StarClasses starClass;
+
+        public enum StarClasses { M, K, G, F, A, B, O };
+
+        public GfxStar(Point location, StarClasses starClass)
+        {
+            this.location = location;
+            this.starClass = starClass;
+        }
+
+        public Point Location
+        {
+            get { return location; }
+            set { this.Location = value; }
+        }
+
+        public Color GetColor()
+        {
+            switch (starClass)
+            {
+                case StarClasses.O:
+                    return Color.FromArgb(58, 144, 255);
+                case StarClasses.B:
+                    return Color.FromArgb(81, 173, 255);
+                case StarClasses.A:
+                    return Color.FromArgb(138, 239, 247);
+                case StarClasses.F:
+                    return Color.FromArgb(203, 255, 242);
+                case StarClasses.G:
+                    return Color.FromArgb(232, 255, 167);
+                case StarClasses.K:
+                    return Color.FromArgb(255, 218, 52);
+                case StarClasses.M:
+                    return Color.FromArgb(255, 73, 0);
+                default:
+                    return Color.Brown;
+            }
+        }
+
+        public int GetSize()
+        {
+            switch (starClass)
+            {
+                case StarClasses.O:
+                    return 12;
+                case StarClasses.B:
+                    return 6;
+                case StarClasses.A:
+                    return 3;
+                case StarClasses.F:
+                    return 2;
+                case StarClasses.G:
+                    return 2;
+                case StarClasses.K:
+                    return 2;
+                default:
+                    return 1;
+            }
+        }
+
+        public void SetClass(StarClasses starClass)
+        {
+            this.starClass = starClass;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            SolidBrush brush = new SolidBrush(GetColor());
+            int size = GetSize();
+            if (size > 4)
+                graphics.FillEllipse(brush, Location.X, Location.Y, size, size);
+            else
+                graphics.FillRectangle(brush, Location.X, Location.Y, size, size);
+        }
+    }
+}
